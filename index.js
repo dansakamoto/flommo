190a7b0247fed2171c914063d8398d6de0202a22
Dan Sakamoto <contact@dansakamoto.com>
2023-08-09 21:21:54 -0400

    Fixed readdir error when no sources

--- index.js
+++ index.js
@@ -15,13 +15,19 @@ const hydraPath = "./public/sources/hydra";
 const p5Path = "./public/sources/p5";
 
 app.get('/srclist', (req, res) => {
-    var vids = fs.readdirSync(vidsPath);
-    var hydras = fs.readdirSync(hydraPath);
-    var p5s = fs.readdirSync(p5Path);
+    var vids = [];
+    var hydras = [];
+    var p5s = [];
 
-    if(vids[0] === ".DS_Store") vids.shift();
-    if(hydras[0] === ".DS_Store") hydras.shift();
-    if(p5s[0] === ".DS_Store") p5s.shift();
+    if(fs.existsSync(vidsPath) && fs.existsSync(hydraPath) && fs.existsSync(p5Path)){
+        vids = fs.readdirSync(vidsPath);
+        hydras = fs.readdirSync(hydraPath);
+        p5s = fs.readdirSync(p5Path);
+
+        if(vids[0] === ".DS_Store") vids.shift();
+        if(hydras[0] === ".DS_Store") hydras.shift();
+        if(p5s[0] === ".DS_Store") p5s.shift();
+    }
 
     res.send({"vids":vids, "hydras":hydras, "p5s":p5s});
 });
