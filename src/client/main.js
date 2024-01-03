ece25480b64ebeccbf72a047b04d1ae096aa2b40
Dan Sakamoto <contact@dansakamoto.com>
2024-01-03 00:43:22 -0500

    fix: jumpy page load
    Added height + width properties to logo element.
    
    closes #57

--- src/client/main.js
+++ src/client/main.js
@@ -7,6 +7,8 @@ const versionNum = "0.2.0";
 document.querySelector("#title").innerHTML = `<img
 style="width: 300px"
 src="${logo}"
+width="300"
+height="85"
 alt="Flommo"
 /><span style="font-family: 'Courier New', Courier, monospace"
 >${versionNum}</span
