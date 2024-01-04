cbc48edd8e10a7f6cd45003e9d484d47899aef21
Dan Sakamoto <contact@dansakamoto.com>
2024-01-04 16:17:04 -0500

    refactor: clean up codeEditor.js

--- src/client/components/codeEditor.js
+++ src/client/components/codeEditor.js
@@ -6,10 +6,10 @@ import { uploadHydra, uploadP5 } from "../utils/sourceManager.js";
 import { resizeRenderer } from "./renderer.js";
 import "./codeEditor.css";
 
-var activePanel = "info";
+var activePanel;
 
-const language = new Compartment(),
-  tabSize = new Compartment();
+const language = new Compartment();
+const tabSize = new Compartment();
 const hydraEditorState = EditorState.create({
   doc: "osc().out()",
   extensions: [
@@ -38,6 +38,19 @@ const p5Editor = new EditorView({
   state: p5EditorState,
   lineNumbers: false,
 });
+
+toggleEditor("title");
+resizeEditor();
+window.addEventListener("resize", resizeEditor);
+document.addEventListener(
+  "keydown",
+  (event) => {
+    if (event.code == "Space" && event.ctrlKey) {
+      toggleEditor("hide");
+    }
+  },
+  false
+);
 
 export function initEditor() {
   document.querySelector("#hydraeditor").addEventListener("keypress", (e) => {
@@ -71,131 +84,65 @@ export function initEditor() {
 
 function resizeEditor() {
   const editorHeight =
-    window.innerHeight - document.getElementById("menu").offsetHeight;
-  let editors = document.querySelectorAll(".cm-editor");
-  for (let e of editors) {
+    window.innerHeight - document.querySelector("#menu").offsetHeight;
+  const editors = document.querySelectorAll(".cm-editor");
+
+  for (const e of editors) {
     e.style.height = editorHeight + "px";
   }
   document.querySelector("#videoeditor").style.height = editorHeight + "px";
 }
-resizeEditor();
-window.addEventListener("resize", resizeEditor);
 
 function toggleEditor(active) {
-  if (active == activePanel) {
-    activePanel = "none";
-
-    document.querySelector("#menu").style = "diplay:flex";
-    document.querySelector("#hydraeditor").style = "display:none;";
-    document.querySelector("#p5editor").style = "display:none;";
-    document.querySelector("#videoeditor").style = "display:none;";
-    document.querySelector("#nocursor").style = "display:none;";
-    document.querySelector("#manual").style = "display:none;";
-    document.querySelector("#empty").style = "display:block;";
-
-    document.querySelector("#hydrabutton").classList.remove("active");
-    document.querySelector("#p5button").classList.remove("active");
-    document.querySelector("#videobutton").classList.remove("active");
-    document.querySelector("#infobutton").classList.remove("active");
-    document.querySelector("#titlebutton").classList.remove("active");
-
-    resizeRenderer();
-
+  if (active === "hide") {
+    if (document.querySelector("#hud").style.visibility == "hidden") {
+      document.querySelector("#hud").style = "visibility:visible";
+      document.querySelector("#interface").style.cursor = "auto";
+    } else {
+      document.querySelector("#hud").style = "visibility:hidden";
+      document.querySelector("#interface").style.cursor = "none";
+    }
     return;
   }
 
-  switch (active) {
-    case "video":
-      activePanel = "video";
-      document.querySelector("#menu").style = "diplay:flex";
-      document.querySelector("#hydraeditor").style = "display:none;";
-      document.querySelector("#p5editor").style = "display:none;";
-      document.querySelector("#videoeditor").style = "display:flex;";
-      document.querySelector("#nocursor").style = "display:none;";
-      document.querySelector("#manual").style = "display:none;";
-      document.querySelector("#empty").style = "display:none;";
+  if (active === activePanel) active = "none";
 
-      document.querySelector("#hydrabutton").classList.remove("active");
-      document.querySelector("#p5button").classList.remove("active");
-      document.querySelector("#videobutton").classList.add("active");
-      document.querySelector("#infobutton").classList.remove("active");
-      document.querySelector("#titlebutton").classList.remove("active");
-      resizeEditor();
-      break;
-    case "p5":
-      activePanel = "p5";
-      document.querySelector("#menu").style = "diplay:flex";
-      document.querySelector("#hydraeditor").style = "display:none;";
-      document.querySelector("#p5editor").style = "display:block;";
-      document.querySelector("#videoeditor").style = "display:none;";
-      document.querySelector("#nocursor").style = "display:none;";
-      document.querySelector("#manual").style = "display:none;";
-      document.querySelector("#empty").style = "display:none;";
+  activePanel = active;
 
-      document.querySelector("#hydrabutton").classList.remove("active");
-      document.querySelector("#p5button").classList.add("active");
-      document.querySelector("#videobutton").classList.remove("active");
-      document.querySelector("#infobutton").classList.remove("active");
-      document.querySelector("#titlebutton").classList.remove("active");
-      resizeEditor();
-      break;
-    case "hydra":
-      activePanel = "hydra";
-      document.querySelector("#menu").style = "diplay:flex";
-      document.querySelector("#hydraeditor").style = "display:block;";
-      document.querySelector("#p5editor").style = "display:none;";
-      document.querySelector("#videoeditor").style = "display:none;";
-      document.querySelector("#nocursor").style = "display:none;";
-      document.querySelector("#manual").style = "display:none;";
-      document.querySelector("#empty").style = "display:none;";
+  document.querySelector("#hydrabutton").classList.remove("active");
+  document.querySelector("#p5button").classList.remove("active");
+  document.querySelector("#videobutton").classList.remove("active");
+  document.querySelector("#infobutton").classList.remove("active");
+  document.querySelector("#titlebutton").classList.remove("active");
+  document.querySelector("#hydraeditor").style = "display:none;";
+  document.querySelector("#p5editor").style = "display:none;";
+  document.querySelector("#videoeditor").style = "display:none;";
+  document.querySelector("#nocursor").style = "display:none;";
+  document.querySelector("#manual").style = "display:none;";
+  document.querySelector("#empty").style = "display:none;";
 
-      document.querySelector("#hydrabutton").classList.add("active");
-      document.querySelector("#p5button").classList.remove("active");
-      document.querySelector("#videobutton").classList.remove("active");
-      document.querySelector("#infobutton").classList.remove("active");
-      document.querySelector("#titlebutton").classList.remove("active");
-      resizeEditor();
-      break;
-    case "info":
-      activePanel = "info";
-      document.querySelector("#hydraeditor").style = "display:none;";
-      document.querySelector("#p5editor").style = "display:none;";
-      document.querySelector("#videoeditor").style = "display:none;";
-      document.querySelector("#nocursor").style = "display:none;";
-      document.querySelector("#manual").style = "display:flex;";
-      document.querySelector("#empty").style = "display:none;";
-
-      document.querySelector("#hydrabutton").classList.remove("active");
-      document.querySelector("#p5button").classList.remove("active");
-      document.querySelector("#videobutton").classList.remove("active");
-      document.querySelector("#infobutton").classList.add("active");
-      document.querySelector("#titlebutton").classList.remove("active");
-      resizeRenderer();
-      break;
-    case "title":
-      activePanel = "title";
-      document.querySelector("#hydraeditor").style = "display:none;";
-      document.querySelector("#p5editor").style = "display:none;";
-      document.querySelector("#videoeditor").style = "display:none;";
-      document.querySelector("#nocursor").style = "display:flex;";
-      document.querySelector("#manual").style = "display:none;";
-      document.querySelector("#empty").style = "display:none;";
-
-      document.querySelector("#hydrabutton").classList.remove("active");
-      document.querySelector("#p5button").classList.remove("active");
-      document.querySelector("#videobutton").classList.remove("active");
-      document.querySelector("#infobutton").classList.remove("active");
-      document.querySelector("#titlebutton").classList.add("active");
-      resizeRenderer();
-      break;
-    case "hide":
-      if (document.querySelector("#hud").style.visibility == "hidden") {
-        document.querySelector("#hud").style = "visibility:visible";
-        document.querySelector("#interface").style.cursor = "auto";
-      } else {
-        document.querySelector("#hud").style = "visibility:hidden";
-        document.querySelector("#interface").style.cursor = "none";
-      }
+  if (active === "hydra") {
+    document.querySelector("#hydrabutton").classList.add("active");
+    document.querySelector("#hydraeditor").style = "display:block";
+    resizeEditor();
+  } else if (active === "p5") {
+    document.querySelector("#p5button").classList.add("active");
+    document.querySelector("#p5editor").style = "display:block";
+    resizeEditor();
+  } else if (active === "video") {
+    document.querySelector("#videobutton").classList.add("active");
+    document.querySelector("#videoeditor").style = "display:flex";
+    resizeEditor();
+  } else if (active === "info") {
+    document.querySelector("#infobutton").classList.add("active");
+    document.querySelector("#manual").style = "display:flex";
+    resizeRenderer();
+  } else if (active === "title") {
+    document.querySelector("#titlebutton").classList.add("active");
+    document.querySelector("#nocursor").style = "display:flex";
+    resizeRenderer();
+  } else if (active === "none") {
+    document.querySelector("#empty").style = "display:block";
+    resizeRenderer();
   }
 }
-toggleEditor("title");
