8befb06c90cf6274b406b397e2f74ee5f1302a4d
Dan Sakamoto <contact@dansakamoto.com>
2024-03-14 15:07:22 -0400

    feat: title page refinements
    Hide or show compatibility warning depending on if Chrome browser is
    detected.
    Update fonts to match the monospace of the rest of the UI.
    
    Closes #96

--- src/client/ui/setupUI.js
+++ src/client/ui/setupUI.js
@@ -3,7 +3,12 @@ import { updateMenu } from "./menu";
 import { updateEditors, resizeEditors } from "./editors";
 import { updateRenderer, resizeRenderer } from "./renderer";
 
+let isChrome = navigator.userAgent.indexOf("Chrome") > -1;
+let warningDiv = document.querySelector("#browser-warning");
+
 export function setupUI() {
+  if (!isChrome) warningDiv.style = "display:block";
+
   updateRenderer();
   updateMixer();
   updateEditors();
