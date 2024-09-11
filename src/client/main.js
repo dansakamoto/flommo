20d71913c966be0a6554489622c61307b72c33df
Dan Sakamoto <contact@dansakamoto.com>
2024-09-11 14:51:13 -0400

    update version number

--- src/client/main.js
+++ src/client/main.js
@@ -3,7 +3,7 @@ import "./main.css";
 import session from "./session";
 import { loadRoomData } from "./services/data";
 
-const versionNum = "0.14.0";
+const versionNum = "0.15.0";
 
 session.initFromURL();
 loadRoomData();
